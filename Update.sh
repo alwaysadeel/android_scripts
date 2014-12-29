da117301ed4a00a984e1f946a3db8846fdb60515
Adeel Javed <alwaysadeel@gmail.com>
2014-12-29 17:58:11 +0500

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,1 +1,3 @@
 git pull && ./repo sync -cq -j8
+
+curl https://raw.githubusercontent.com/CyanogenMod/android_hardware_qcom_wlan/cm-12.0/qcwcn/config/android_dhcpcd.conf > device/sony/montblanc-common/config/dhcpcd.conf
