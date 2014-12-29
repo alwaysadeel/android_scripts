a03cf988b032a3187fe9d36604e25d768c40fa6f
Adeel Javed <alwaysadeel@gmail.com>
2014-12-29 14:03:51 +0500

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,7 +1,1 @@
 git pull && ./repo sync -cq -j8
-
-# Dhcpd rules needed by Wi-Fi Direct
-curl https://raw.githubusercontent.com/CyanogenMod/android_hardware_qcom_wlan/cm-12.0/qcwcn/config/android_dhcpcd.conf > device/sony/montblanc-common/config/dhcpcd.conf
-
-# APNs list
-curl https://raw.githubusercontent.com/CyanogenMod/android_vendor_cm/cm-12.0/prebuilt/common/etc/apns-conf.xml > device/generic/goldfish/data/etc/apns-conf.xml
